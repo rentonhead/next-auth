0e7e896efb435b069518dfdfad84fb9948dc0e8a
Hasan Cemil Acar <70777645+rentonhead@users.noreply.github.com>
2023-08-20 04:32:00 +0300

    complete -rentboy

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,3 +1,11 @@
-export default function Register() {
-  return <div>Register</div>;
+import RegisterForm from "@/components/RegisterForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export default async function Register() {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/dashboard");
+  return <RegisterForm />;
 }
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,38 +1,77 @@
 "use client";
+
 import Link from "next/link";
 import { useState } from "react";
-import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-export default function LoginForm() {
+export default function RegisterForm() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
   const router = useRouter();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!name || !email || !password) {
+      setError("Please Fill All The Fields");
+      return;
+    }
+
     try {
-      const res = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
+      const resUserExists = await fetch("api/userExists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
       });
-      if (res.error) {
-        setError("Invalid Credentials");
+
+      const { user } = await resUserExists.json();
+
+      if (user) {
+        setError("User Already Exists");
         return;
       }
-      router.replace("dashboard");
+
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+        router.push("/");
+      } else {
+        console.log("Error");
+      }
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
         <h1 className="text-xl font-bold my-4">Enter The Details</h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+          />
           <input
             onChange={(e) => setEmail(e.target.value)}
             type="text"
@@ -44,20 +83,20 @@ export default function LoginForm() {
             placeholder="Password"
           />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-            Login
+            Register
           </button>
-
           {error && (
             <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
           )}
+
           <Link
             className="
-          text-sm mt-3 text-right"
-            href={"/register"}
+    text-sm mt-3 text-right"
+            href={"/"}
           >
-            Don't have an account? <span className="underline">Register</span>
+            Already Have an Account? <span className="underline">Login</span>
           </Link>
         </form>
       </div>
